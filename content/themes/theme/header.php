1cd1275b3faca5a4f70d50a3076ecd0961e77d67
All07-07 <96721730+All07-07@users.noreply.github.com>
2021-12-27 16:08:57 +0100

    Create header.php

---
+++ content/themes/theme/header.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+<!DOCTYPE html>
+<!--[if IE 8]>    <html class="ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9]>    <html class="ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if (gt IE 9)|!(IE)] <?php language_attributes(); ?>><![endif]-->
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!-- feeds & pingback -->
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php do_action('theme_critical_css');?>
+<?php wp_head(); ?>
+</head>
